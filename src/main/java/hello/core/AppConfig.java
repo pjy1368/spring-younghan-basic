098b6d35dbc6a5fea9921961f7a89abf758f6b9a
JinYoung Park <basejin0810@naver.com>
2021-06-12 17:50:29 +0900

    refactor(AppConfig): 할인 정책 변경

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,6 @@
 package hello.core;
 
-import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -21,7 +21,7 @@ public class AppConfig {
         return new MemoryMemberRepository();
     }
 
-    private FixDiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+    private RateDiscountPolicy discountPolicy() {
+        return new RateDiscountPolicy();
     }
 }

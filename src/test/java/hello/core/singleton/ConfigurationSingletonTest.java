2d4c104390202a6607c7089b76449d7f99512966
JinYoung Park <basejin0810@naver.com>
2021-06-15 20:27:47 +0900

    feat: Configuration 관련 테스트 작성

---
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,32 @@
+package hello.core.singleton;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberServiceImpl;
+import hello.core.order.OrderServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ConfigurationSingletonTest {
+
+    @Test
+    void configurationTest() {
+        final ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        final MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        final OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        final MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+
+        assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
+        assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
+    }
+
+    @Test
+    void configurationDeep() {
+        final ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        final AppConfig bean = ac.getBean(AppConfig.class);
+        System.out.println("bean = " + bean.getClass()); // AppConfig CGLIB
+    }
+}

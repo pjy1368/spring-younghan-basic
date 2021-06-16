4af2586dc7f865d25489e8b3d1b7986c2b5ff3d5
JinYoung Park <basejin0810@naver.com>
2021-06-16 23:33:34 +0900

    refactor: 롬복 적용

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,15 +1,13 @@
 package hello.core.member;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+@RequiredArgsConstructor
 @Component
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
-
-    public MemberServiceImpl(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
 
     @Override
     public void join(Member member) {

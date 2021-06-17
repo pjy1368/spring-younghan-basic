aae89ba11ab7c73cd52c6ab530b35c472be6c403
JinYoung Park <basejin0810@naver.com>
2021-06-17 17:20:45 +0900

    refactor: 프록시 속성을 이용하여 스코프 코드 수정

--- src/main/java/hello/core/web/LogDemoService.java
+++ src/main/java/hello/core/web/LogDemoService.java
@@ -2,16 +2,15 @@ package hello.core.web;
 
 import hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class LogDemoService {
 
-    private final ObjectProvider<MyLogger> myLoggerObjectProvider;
+    private final MyLogger myLogger;
 
     public void logic(String id) {
-        myLoggerObjectProvider.getObject().log("service id = " + id);
+        myLogger.log("service id = " + id);
     }
 }
